af17fe7b2cb8d7f9221b42ae06f8df66c6c218d8
Luka Deruyck <l.deruyck@outlook.fr>
2021-03-07 15:06:46 +0100

    Ajoutez des fichiers projet.

---
+++ Json_Gen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Console_LookZay
+{
+    class Json_Gen
+    {
+
+        public const String json_figlet = @"      _  _____  ____  _   _  _____ ______ _   _ ______ _____         _______ ____  _____  
+     | |/ ____|/ __ \| \ | |/ ____|  ____| \ | |  ____|  __ \     /\|__   __/ __ \|  __ \ 
+     | | (___ | |  | |  \| | |  __| |__  |  \| | |__  | |__) |   /  \  | | | |  | | |__) |
+ _   | |\___ \| |  | | . ` | | |_ |  __| | . ` |  __| |  _  /   / /\ \ | | | |  | |  _  / 
+| |__| |____) | |__| | |\  | |__| | |____| |\  | |____| | \ \  / ____ \| | | |__| | | \ \ 
+ \____/|_____/ \____/|_| \_|\_____|______|_| \_|______|_|  \_\/_/    \_\_|  \____/|_|  \_\";
+
+        public static void createJsonItemBasic(StreamWriter sw, String textureName)
+        {
+            sw.WriteLine("{");
+            sw.WriteLine("    \"parent\": \"builtin/generated\",");
+            sw.WriteLine("    \"textures\": {");
+            sw.WriteLine("        \"layer0\": \"items/" + textureName + "\"");
+            sw.WriteLine("    },");
+            sw.WriteLine("    \"display\": {");
+            sw.WriteLine("        \"thirdperson\": {");
+            sw.WriteLine("            \"rotation\": [ -90, 0, 0 ],");
+            sw.WriteLine("            \"translation\": [ 0, 1, -3 ],");
+            sw.WriteLine("            \"scale\": [ 0.55, 0.55, 0.55 ]");
+            sw.WriteLine("        },");
+            sw.WriteLine("        \"firstperson\": {");
+            sw.WriteLine("            \"rotation\": [ 0, -135, 25 ],");
+            sw.WriteLine("            \"translation\": [ 0, 4, 2 ],");
+            sw.WriteLine("            \"scale\": [ 1.7, 1.7, 1.7 ]");
+            sw.WriteLine("        }");
+            sw.WriteLine("    }");
+            sw.WriteLine("}");
+            sw.Close();
+        }
+        public static void createJsonItemTools(StreamWriter sw, String textureName)
+        {
+            sw.WriteLine("{");
+            sw.WriteLine("    \"parent\": \"builtin/generated\",");
+            sw.WriteLine("    \"textures\": {");
+            sw.WriteLine("        \"layer0\": \"items/" + textureName + "\"");
+            sw.WriteLine("    },");
+            sw.WriteLine("    \"display\": {");
+            sw.WriteLine("        \"thirdperson\": {");
+            sw.WriteLine("            \"rotation\": [ 0, 90, -35 ],");
+            sw.WriteLine("            \"translation\": [ 0, 1.25, -3.5 ],");
+            sw.WriteLine("            \"scale\": [ 0.85, 0.85, 0.85 ]");
+            sw.WriteLine("        },");
+            sw.WriteLine("        \"firstperson\": {");
+            sw.WriteLine("            \"rotation\": [ 0, -135, 25 ],");
+            sw.WriteLine("            \"translation\": [ 0, 4, 2 ],");
+            sw.WriteLine("            \"scale\": [ 1.7, 1.7, 1.7 ]");
+            sw.WriteLine("        }");
+            sw.WriteLine("    }");
+            sw.WriteLine("}");
+            sw.Close();
+        }
+
+        public static void createJsonBlockStates(StreamWriter sw, String blockName)
+        {
+            sw.WriteLine("{");
+            sw.WriteLine("    \"variants\": {");
+            sw.WriteLine("        \"normal\": { \"model\": \"" + blockName + "\" }");
+            sw.WriteLine("}");
+            sw.Close();
+        }
+
+        public static void createJsonBlock(StreamWriter sw, String textureName)
+        {
+            sw.WriteLine("{");
+            sw.WriteLine("    \"parent\": \"block/cube_all\",");
+            sw.WriteLine("    \"textures\": {");
+            sw.WriteLine("        \"all\": \"block/" + textureName + "\"");
+            sw.WriteLine("    }");
+            sw.WriteLine("}");
+            sw.Close();
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Console_LookZay
+{
+    class Main
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "JSON Creator";
+            mainmenu();
+        }
+
+        public static void headerJsonCreator(ConsoleColor color)
+        {
+            Console.Clear();
+            Console.ForegroundColor = color;
+            Console.WriteLine(Json_Gen.json_figlet);
+            Console.WriteLine("");
+        }
+
+        static void mainmenu()
+        {
+            headerJsonCreator(ConsoleColor.White);
+            Console.WriteLine("[1] Item Creator");
+            Console.WriteLine("[2] Block Creator");
+            Console.WriteLine("[3] Credits");
+            Console.WriteLine("[4] Exit");
+            Console.WriteLine("");
+            Console.Write("Choose Generator : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                itemgenerator();
+            }
+            else if (input == "2")
+            {
+                blockgenerator();
+            }
+            else if (input == "3")
+            {
+                credits();
+            }
+            else if (input == "4")
+            {
+                exit();
+            }
+            else
+            {
+                error_number();
+            }
+        }
+
+        public static void itemgenerator()
+        {
+            headerJsonCreator(ConsoleColor.White);
+            Console.WriteLine("[1] Is Basic Item");
+            Console.WriteLine("[2] Is Tool");
+            Console.WriteLine("[3] Home");
+            Console.WriteLine("");
+            Console.Write("Choose Item Options : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                itemgenerator_basic();
+            }
+
+            if (input == "2")
+            {
+                itemgenerator_tools();
+            }
+
+            if (input == "3")
+            {
+                mainmenu();
+            }
+
+            else
+            {
+                error_number();
+            }
+        }
+
+        public static void itemgenerator_basic()
+        {
+            headerJsonCreator(ConsoleColor.White);
+            Console.Write("Texture Name : ");
+            string textureName = Console.ReadLine();
+            Console.Write("Item Name : ");
+            string itemName = Console.ReadLine();
+
+            String path_export = "./export";
+            String path_models = "./export/models/";
+            String path_item = "./export/models/item/";
+
+            DirectoryInfo di = Directory.CreateDirectory(path_export);
+            DirectoryInfo di1 = Directory.CreateDirectory(path_models);
+            DirectoryInfo di2 = Directory.CreateDirectory(path_item);
+
+            StreamWriter sw = new StreamWriter(@"./export/models/item/" + itemName + ".json");
+            Json_Gen.createJsonItemBasic(sw, textureName);
+            successfully_item(itemName, false);
+        }
+
+
+        public static void itemgenerator_tools()
+        {
+            headerJsonCreator(ConsoleColor.White);
+            Console.Write("Texture Name : ");
+            string textureName = Console.ReadLine();
+            Console.Write("Item Name : ");
+            string itemName = Console.ReadLine();
+
+            String path_export = "./export";
+            String path_models = "./export/models/";
+            String path_item = "./export/models/item/";
+
+            DirectoryInfo di = Directory.CreateDirectory(path_export);
+            DirectoryInfo di1 = Directory.CreateDirectory(path_models);
+            DirectoryInfo di2 = Directory.CreateDirectory(path_item);
+
+            StreamWriter sw = new StreamWriter(@"./export/models/item/" + itemName + ".json");
+            Json_Gen.createJsonItemTools(sw, textureName);
+            successfully_item(itemName, true);
+        }
+
+        public static void blockgenerator_basic()
+        {
+            headerJsonCreator(ConsoleColor.White);
+            Console.Write("Texture Name : ");
+            string textureName = Console.ReadLine();
+            Console.Write("Block Name : ");
+            string blockName = Console.ReadLine();
+
+            String path_export = "./export";
+            String path_blockstates = "./export/blockstates/";
+            String path_models = "./export/models/";
+            String path_block = "./export/models/block/";
+
+            DirectoryInfo di = Directory.CreateDirectory(path_export);
+            DirectoryInfo di1 = Directory.CreateDirectory(path_blockstates);
+            DirectoryInfo di2 = Directory.CreateDirectory(path_models);
+            DirectoryInfo di3 = Directory.CreateDirectory(path_block);
+
+            StreamWriter sw = new StreamWriter(@"./export/models/block/" + blockName + ".json");
+            StreamWriter bw = new StreamWriter(@"./export/blockstates/" + blockName +".json");
+            Json_Gen.createJsonBlock(sw, textureName);
+            Json_Gen.createJsonBlockStates(bw, blockName);
+            successfully_block(blockName);
+        }
+
+        public static void successfully_block(String blockName)
+        {
+            headerJsonCreator(ConsoleColor.Green);
+            Console.WriteLine("Block " + blockName + " is successfully created !");
+            Console.WriteLine("");
+            Console.WriteLine("[1] Home");
+            Console.WriteLine("[2] Exit");
+            Console.WriteLine("");
+            Console.Write("Choose Options : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                mainmenu();
+            }
+            else if (input == "2")
+            {
+                exit();
+            }
+            else
+            {
+                error_number();
+            }
+        }
+
+        public static void successfully_item(String itemName, Boolean isTool)
+        {
+            headerJsonCreator(ConsoleColor.Green);
+            if (isTool == true)
+            {
+                Console.WriteLine("Item Tool " + itemName + " is successfully created !");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Item " + itemName + " is successfully created !");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("[1] Home");
+            Console.WriteLine("[2] Exit");
+            Console.WriteLine("");
+            Console.Write("Choose Options : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                mainmenu();
+            }
+            else if (input == "2")
+            {
+                exit();
+            }
+            else
+            {
+                error_number();
+            }
+        }
+
+        public static void error_number()
+        {
+            headerJsonCreator(ConsoleColor.Red);
+            Console.WriteLine("Error, this number is not valid!");
+            Console.WriteLine("");
+            Console.WriteLine("[1] Home");
+            Console.WriteLine("[2] Exit");
+            Console.WriteLine("");
+            Console.Write("Choose Options : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                mainmenu();
+            }
+            else if (input == "2")
+            {
+                exit();
+            }
+            else
+            {
+                error_number();
+            }
+        }
+
+        public static void soon()
+        {
+            headerJsonCreator(ConsoleColor.Red);
+            Console.WriteLine("Error, It's coming soon !");
+            Console.WriteLine("");
+            Console.WriteLine("[1] Home");
+            Console.WriteLine("[2] Exit");
+            Console.WriteLine("");
+            Console.Write("Choose Options : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                mainmenu();
+            }
+            else if (input == "2")
+            {
+                exit();
+            }
+            else
+            {
+                error_number();
+            }
+        }
+
+
+        public static void blockgenerator()
+        {
+            headerJsonCreator(ConsoleColor.White);
+            Console.WriteLine("[1] Is Basic Block");
+            Console.WriteLine("[2] S.O.O.N");
+            Console.WriteLine("[3] Home");
+            Console.WriteLine("");
+            Console.Write("Choose Item Options : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                blockgenerator_basic();
+            }
+
+            if (input == "2")
+            {
+                blockgenerator();
+            }
+
+            if (input == "3")
+            {
+                mainmenu();
+            }
+
+            else
+            {
+                error_number();
+            }
+        }
+
+        public static void credits()
+        {
+            headerJsonCreator(ConsoleColor.Blue);
+            Console.WriteLine("This Json creator was developed by LookZay");
+            Console.WriteLine("");
+            Console.WriteLine("My Discord : LookZay#1182");
+            Console.WriteLine("My Github : LookZay");
+            Console.WriteLine("");
+            Console.WriteLine("[1] Home");
+            Console.WriteLine("[2] Exit");
+            Console.WriteLine("");
+            Console.Write("Choose Options : ");
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                mainmenu();
+            }
+            else if (input == "2")
+            {
+                exit();
+            }
+            else
+            {
+                error_number();
+            }
+        }
+
+        public static void exit()
+        {
+            headerJsonCreator(ConsoleColor.Green);
+            Console.WriteLine("Bye !");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Thread.Sleep(1000);
+            Environment.Exit(0);
+        }
+    }
+}
